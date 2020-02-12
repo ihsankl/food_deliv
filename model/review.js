fcd9676046187c3f2a8e5b9acf9a61b2b0c32307
Ihsan kurniawan <ikurniawanlubis@gmail.com>
2020-02-12 23:10:50 +0700

    update

--- model/review.js
+++ model/review.js
@@ -1,6 +1,6 @@
 const review = {
-  detailWithUser: 'SELECT review.id, review.review, users.username, items.name, items.id AS itemID, review.ratings, review.created_on, review.updated_on FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id WHERE review.user = ?', // #1
-  get: 'SELECT review.review, users.username, items.name, review.ratings, review.created_on, review.updated_on FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id', // #2
+  detailWithUser: 'SELECT review.id, review.review, users.username, items.name, items.id AS itemID, review.ratings, review.created_on, review.updated_on, items.images FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id WHERE review.user = ?', // #1
+  get: 'SELECT review.review, users.username, items.name, items.images, review.ratings, review.created_on, review.updated_on FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id', // #2
   detail: 'SELECT review.review, users.username, items.name, review.ratings, review.created_on, review.updated_on FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id WHERE review.id = ?',
   insert: 'INSERT INTO review ( review, user, item, ratings, created_on, updated_on ) VALUES(?, ?, ?, ?, ?, ?)',
   find: 'SELECT AVG(ratings) AS avg_rate FROM review WHERE item = ?',

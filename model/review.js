bf9dd55c5cf54aee346d5cbd2278d01da97c93bb
Ihsan kurniawan <ikurniawanlubis@gmail.com>
2020-02-13 08:23:54 +0700

    update

--- model/review.js
+++ model/review.js
@@ -1,7 +1,7 @@
 const review = {
   detailWithUser: 'SELECT review.id, review.review, users.username, items.name, items.id AS itemID, review.ratings, review.created_on, review.updated_on, items.images FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id WHERE review.user = ?', // #1
   get: 'SELECT review.review, users.username, items.name, items.images, review.ratings, review.created_on, review.updated_on FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id', // #2
-  detail: 'SELECT review.review, review.id, users.username, items.name,items.images, review.ratings, review.created_on, review.updated_on FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id WHERE review.id = ?',
+  detail: 'SELECT review.review, review.id, users.username, items.name, items.id as itemID, items.images, review.ratings, review.created_on, review.updated_on FROM review INNER JOIN users ON review.user = users.id INNER JOIN items ON review.item = items.id WHERE review.id = ?',
   insert: 'INSERT INTO review ( review, user, item, ratings, created_on, updated_on ) VALUES(?, ?, ?, ?, ?, ?)',
   find: 'SELECT AVG(ratings) AS avg_rate FROM review WHERE item = ?',
   insertAvg: 'UPDATE items SET total_ratings=? WHERE id = ?',
